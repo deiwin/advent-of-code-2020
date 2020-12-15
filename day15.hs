b74b6d94055259b18510c34eac0907b98d2cc9d2
Deiwin Sarjas <deiwin.sarjas@gmail.com>
2020-12-15 08:18:30 +0200

    First optimization attempt, but still too slow

--- day15.hs
+++ day15.hs
@@ -30,6 +30,10 @@ import qualified Data.IntSet                   as S
 import           Data.Bits                      ( setBit
                                                 , clearBit
                                                 )
+import           Criterion.Main                 ( defaultMain
+                                                , bench
+                                                , whnf
+                                                )
 import           Debug.Trace                    ( traceShowId )
 
 memoryGame :: [Int] -> [Int]
@@ -39,12 +43,31 @@ memoryGame xs@(x : rest) = case elemIndex x rest of
     Just i  -> (i + 1) : memoryGame ((i + 1) : xs)
 
 nthMemoryGame :: Int -> [Int] -> Int
-nthMemoryGame n start = memoryGame (reverse start) !! (n - length start - 1)
+nthMemoryGame n start = go initialIx (last start) initialMap
+    where
+        -- key: the number
+        -- val: its most recent index
+        initialMap = M.fromList $ zip (init start) [0..]
+        initialIx = length start
+        go :: Int -> Int -> IntMap Int -> Int
+        go ix lastVal map
+          | ix == (n - 1) = newVal
+          | otherwise = go (ix + 1) newVal newMap
+            where
+                (maybeLastIx, newMap) = M.insertLookupWithKey constWithKey lastVal (ix - 1) map
+                constWithKey k new old = new
+                newVal = case maybeLastIx of
+                           Nothing -> 0
+                           Just lastIx -> ix - lastIx - 1
 
 solve :: [Int] -> Int
 solve = nthMemoryGame 2020
 
+solve2 :: [Int] -> Int
+solve2 = nthMemoryGame 30000000
+
 main = do
+    let input = [8, 11, 0, 19, 1, 2]
     runTestTT $ TestCase $ do
         take 10 (memoryGame [6, 3, 0]) @?= [0, 3, 3, 1, 0, 4, 0, 2, 0, 2]
         nthMemoryGame 9 [0, 3, 6] @?= 4
@@ -55,4 +78,11 @@ main = do
         solve [2, 3, 1] @?= 78
         solve [3, 2, 1] @?= 438
         solve [3, 1, 2] @?= 1836
-        solve [8, 11, 0, 19, 1, 2] @?= 447
+        solve input @?= 447
+        -- solve2 [0, 3, 6] @?= 175593
+        -- solve2 input @?= 447
+    defaultMain
+        [ bench "10^3" (whnf (nthMemoryGame 1000) input)
+        , bench "10^4" (whnf (nthMemoryGame 10000) input)
+        , bench "10^5" (whnf (nthMemoryGame 100000) input)
+        ]
