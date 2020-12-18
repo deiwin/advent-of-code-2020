8225f983175719184af186f01f65ab4335be9e6d
Deiwin Sarjas <deiwin.sarjas@gmail.com>
2020-12-18 08:49:49 +0200

    Add a template with some common imports

---
+++ dayXX.hs
@@ -0,0 +1,75 @@
+#!/usr/bin/env stack
+-- stack --resolver lts-15.6 runghc --package HUnit --package text
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE PartialTypeSignatures #-}
+
+import           Prelude                 hiding ( readFile )
+import           Data.Text                      ( Text )
+import           Data.Text.IO                   ( readFile )
+import           Test.HUnit.Text                ( runTestTT )
+import           Test.HUnit.Base                ( Test(TestCase)
+                                                , (@?=)
+                                                )
+import           Criterion.Main                 ( defaultMain
+                                                , bench
+                                                , whnf
+                                                )
+import           Debug.Trace                    ( traceShow
+                                                , traceShowId
+                                                )
+import qualified Text.Megaparsec               as P
+import qualified Text.Megaparsec.Char          as P
+import qualified Text.Megaparsec.Char.Lexer    as PL
+import           Data.Set                       ( Set )
+import qualified Data.Set                      as S
+import           Data.IntSet                    ( IntSet )
+import qualified Data.IntSet                   as IS
+import           Data.Map.Strict                ( Map )
+import qualified Data.Map.Strict               as M
+import           Data.IntMap                    ( IntMap )
+import qualified Data.IntMap                   as IM
+import           Data.Ix                        ( range )
+import           Linear.V2                      ( V2(..) )
+import           Linear.V3                      ( V3(..) )
+import           Linear.V4                      ( V4(..) )
+import           Control.Applicative            ( empty )
+import           Data.Void                      ( Void )
+import           Data.List                      ( foldl1'
+                                                , foldl'
+                                                , isPrefixOf
+                                                , iterate
+                                                )
+import           Data.Ord                       ( comparing )
+import           Data.Function                  ( (&) )
+import           Control.Arrow                  ( (>>>), second )
+import           Data.Maybe                     ( isJust
+                                                , catMaybes
+                                                )
+import           Control.Monad                  ( guard )
+
+type Parser = P.Parsec Void Text
+
+parse :: Text -> _
+parse input = case P.parse parser "" input of
+    Left  bundle -> error (P.errorBundlePretty (bundle :: P.ParseErrorBundle Text Void))
+    Right result -> result
+  where
+    parser = do
+        foo <- P.some P.letterChar
+        return foo
+    spaceConsumer :: Parser ()
+    spaceConsumer = PL.space (P.skipSome (P.char ' ')) empty empty
+    lexeme        = PL.lexeme spaceConsumer
+    symbol        = PL.symbol spaceConsumer
+    number        = lexeme PL.decimal
+    singleEol :: Parser (P.Tokens Text)
+    singleEol = P.eol <* P.notFollowedBy P.eol
+
+main = do
+    input <- readFile "inputs/dayXX.txt"
+    -- exampleInput <- readFile "inputs/dayXX_example.txt"
+    -- print $ parse input
+    runTestTT $ TestCase $ do
+        1 @?= 1
+        1 @?= 1
